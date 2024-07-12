745429d799cd7bbf3afa365e35dd9436e48e4769
Camila <alvescamila87@gmail.com>
2024-07-12 19:29:10 -0300

    load balacing with eureka-server

--- payroll-api/src/main/java/br/com/camila/payroll_api/feignClients/UserFeign.java
+++ payroll-api/src/main/java/br/com/camila/payroll_api/feignClients/UserFeign.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 // Feign como se fosse um proxy
 // Não usamos resttemplate, pois estamos trabalhando com microsserviços (com Feign Client)
-@FeignClient(name = "user-api", url = "http://localhost:8000")
+@FeignClient(name = "user-api")
 public interface UserFeign {
 
     @GetMapping(value = "/api/users/{id}")
--- user-api/src/main/java/br/com/camila/user_api/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/camila/user_api/services/impl/UserServiceImpl.java
@@ -5,20 +5,24 @@ import br.com.camila.user_api.repositories.UserRepository;
 import br.com.camila.user_api.services.UserService;
 import br.com.camila.user_api.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
-
     private final UserRepository userRepository;
+    private final Environment environment;
 
     @Override
     public User findByID(Long id) {
+        log.info("USER_SERVICE ::: Get request on " + environment.getProperty("local.server.port") + " port");
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
